f1f7822015b09950643ffb168e9fd75a25e191ef
Duc Nguyen Van <nguyen.van.ducc@framgia.com>
2019-03-03 12:10:29 +0700

    Initial Project Commit

---
+++ app/src/main/java/com/example/streamingvideoapp/PlayingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.streamingvideoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_playing)
+    }
+}
